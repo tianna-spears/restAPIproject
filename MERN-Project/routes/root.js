5496a73f3443ba55f2beb04bcc0d45d8c3651072
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-05-30 21:46:36 +0100

    lesson 1 of MERN Stack Project

---
+++ MERN-Project/routes/root.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+})
+
+router.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+})
+
+router.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+})
+
+module.exports = router
